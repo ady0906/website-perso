2fa66d725cb4c06aefef4c1515e962d0cf0a4ba3
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-04-29 22:25:02 -0400

    defining variables before adding URLs

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-const express = require('express');
-const nodemailer = require("nodemailer");
-const app = express();
+var path = require("path");
+var express = require("express");
+var fs = require('fs');
+var logger = require("morgan");
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
+var bodyParser = require('body-parser');
+var nconf = require('nconf');
+var auth = require('.config/json');
+const port = process.ENV.PORT || 8081;
 
-app.listen(3000,function(){
-console.log("Express Started on Port 3000");
-});
+var app = express();
+var server = require('http').createServer(app);
+
+
+//including client side and using bodyParser
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' });
+app.use(logger('dev'));
+app.use(logger('combined', { stream: accessLogStream }));
+
+// app.listen(3000,function(){
+// console.log("Express Started on Port 3000");
+// });
